f03947fdc0617313ad03bbaadd37a9ebcd4605ec
S3LT <91411026+S3LTDev@users.noreply.github.com>
2024-04-19 10:34:27 +0100

    Update connection2.php

--- func/connection2.php
+++ func/connection2.php
@@ -1,4 +1,15 @@
 <?php
+/*  
+@@@@@@ @@@@@@  @@@      @@@@@@@      @@@@@@@  @@@@@@@@ @@@  @@@
+@@         @@! @@!        @@!        @@!  @@@ @@!      @@!  @@@
+!@@!!   @!!!:  @!!        @!!        @!@  !@! @!!!:!   @!@  !@!
+   !:!     !!: !!:        !!:        !!:  !!! !!:       !: .:! 
+::.: :  ::: ::  : ::.: :    :         :: :  :  : :: :::    ::   
+
+link  https://github.com/S3LTDev/S3LT-Social-Template
+author  max2tz https://github.com/S3LT
+license  GPL-3.0 License
+*/
 $host = "";
 $user = "";
 $pass = "";
