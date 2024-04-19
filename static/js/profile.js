29f1497e9999ef53dbf37dd14bba26b88074dc7c
S3LT <91411026+S3LTDev@users.noreply.github.com>
2024-04-19 10:40:04 +0100

    Update profile.js

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,3 +1,14 @@
+/*  
+@@@@@@ @@@@@@  @@@      @@@@@@@      @@@@@@@  @@@@@@@@ @@@  @@@
+@@         @@! @@!        @@!        @@!  @@@ @@!      @@!  @@@
+!@@!!   @!!!:  @!!        @!!        @!@  !@! @!!!:!   @!@  !@!
+   !:!     !!: !!:        !!:        !!:  !!! !!:       !: .:! 
+::.: :  ::: ::  : ::.: :    :         :: :  :  : :: :::    ::   
+
+link  https://github.com/S3LTDev/S3LT-Social-Template
+author  max2tz https://github.com/S3LT
+license  GPL-3.0 License
+*/
 let editMode = false
 
 function convertEditMode(mode) {
