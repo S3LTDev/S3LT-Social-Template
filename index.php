d96e7e25c5e372d9f7e28e1d95aeab7646b7a21e
S3LT <91411026+S3LTDev@users.noreply.github.com>
2024-04-19 10:25:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+/*  
+@@@@@@ @@@@@@  @@@      @@@@@@@      @@@@@@@  @@@@@@@@ @@@  @@@
+@@         @@! @@!        @@!        @@!  @@@ @@!      @@!  @@@
+!@@!!   @!!!:  @!!        @!!        @!@  !@! @!!!:!   @!@  !@!
+   !:!     !!: !!:        !!:        !!:  !!! !!:       !: .:! 
+::.: :  ::: ::  : ::.: :    :         :: :  :  : :: :::    ::   
 
+link  https://github.com/S3LTDev/S3LT-Social-Template
+author  max2tz https://github.com/S3LT
+license  GPL-3.0 License
+*/
 session_start();
 
 require_once './vendor/prototype/autoload.php';
